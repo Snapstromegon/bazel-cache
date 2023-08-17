ec5ac303b0a95a4afb022df9f4323ba772d65440
Raphael Höser <raphael.hoeser@aptiv.com>
2023-08-17 17:13:28 +0200

    Move backend out to own module

---
+++ src/backend/http.rs
@@ -0,0 +1,102 @@
+use std::{sync::Arc};
+
+use axum::{
+    body::Bytes,
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+};
+use tokio::{signal, sync::RwLock};
+
+use crate::storage::Storage;
+
+pub struct Backend {}
+
+impl Backend {
+    pub async fn get_action<Store: Storage + Sync + Send>(
+        Path(path): Path<String>,
+        State(cache): State<Arc<RwLock<Store>>>,
+    ) -> Result<Vec<u8>, StatusCode> {
+        if let Some(data) = cache.read().await.get(&format!("ac/{path}")).await {
+            tracing::info!("Responding from cache for ac/{}", path);
+            Ok(data)
+        } else {
+            tracing::info!("ac/{} not in cache", path);
+            Err(StatusCode::NOT_FOUND)
+        }
+    }
+    pub async fn put_action<Store: Storage + std::marker::Sync>(
+        Path(path): Path<String>,
+        State(storage): State<Arc<RwLock<Store>>>,
+        body: Bytes,
+    ) -> impl IntoResponse {
+        if storage.read().await.has(&format!("ac/{path}")).await {
+            StatusCode::CREATED
+        } else {
+            tracing::info!("Storing in cache for ac/{}", path);
+            storage
+                .write()
+                .await
+                .set(&format!("ac/{path}"), body.to_vec())
+                .await;
+            StatusCode::CREATED
+        }
+    }
+
+    pub async fn get_item<Store: Storage>(
+        Path(path): Path<String>,
+        State(storage): State<Arc<RwLock<Store>>>,
+    ) -> impl IntoResponse {
+        if let Some(data) = storage.read().await.get(&format!("cas/{path}")).await {
+            tracing::info!("Responding from cache for cas/{}", path);
+            Ok(data)
+        } else {
+            tracing::info!("cas/{} not in cache", path);
+            Err((StatusCode::NOT_FOUND, "not found"))
+        }
+    }
+
+    pub async fn put_item<Store: Storage + std::marker::Sync>(
+        Path(path): Path<String>,
+        State(storage): State<Arc<RwLock<Store>>>,
+        body: Bytes,
+    ) -> impl IntoResponse {
+        tracing::info!("Storing in cache for cas/{}", path);
+        if storage.read().await.has(&format!("ac/{path}")).await {
+            StatusCode::CREATED
+        } else {
+            storage
+                .write()
+                .await
+                .set(&format!("cas/{path}"), body.to_vec())
+                .await;
+            StatusCode::CREATED
+        }
+    }
+
+    pub async fn shutdown_signal() {
+        let ctrl_c = async {
+            signal::ctrl_c()
+                .await
+                .expect("failed to install Ctrl+C handler");
+        };
+
+        #[cfg(unix)]
+        let terminate = async {
+            signal::unix::signal(signal::unix::SignalKind::terminate())
+                .expect("failed to install signal handler")
+                .recv()
+                .await;
+        };
+
+        #[cfg(not(unix))]
+        let terminate = std::future::pending::<()>();
+
+        tokio::select! {
+            _ = ctrl_c => {},
+            _ = terminate => {},
+        }
+
+        println!("shutting down");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
 #![warn(clippy::pedantic)]
 #![feature(async_fn_in_trait)]
 
-use axum::{
-    body::Bytes,
-    extract::{DefaultBodyLimit, Path, State},
-    http::StatusCode,
-    response::IntoResponse,
-    routing::get,
-    Router,
-};
+use axum::{extract::DefaultBodyLimit, routing::get, Router};
 use s3::bucket::Bucket;
 use s3::creds::Credentials;
 use s3::region::Region;
 use std::{net::SocketAddr, sync::Arc};
-use tokio::{signal, sync::RwLock};
+use tokio::sync::RwLock;
 
 mod storage;
-use storage::{Storage, S3Store, InMemoryStore};
+use storage::{InMemoryStore, S3Store};
+mod backend;
+use backend::Backend;
 
 #[tokio::main]
 async fn main() {
@@ -26,48 +21,22 @@ async fn main() {
     let _storage = create_s3_storage();
     let storage = Arc::new(RwLock::new(InMemoryStore::new()));
 
-    // build our application with a route
     let app = Router::new()
-        .route("/ac/*path", get(get_action).put(put_action))
-        .route("/cas/*path", get(get_item).put(put_item))
-        .with_state(storage)
+        .route(
+            "/ac/*path",
+            get(Backend::get_action).put(Backend::put_action),
+        )
+        .route("/cas/*path", get(Backend::get_item).put(Backend::put_item))
+        .with_state(storage.clone())
         .layer(DefaultBodyLimit::max(1024 * 1024 * 1024));
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
-        .with_graceful_shutdown(shutdown_signal())
+        .with_graceful_shutdown(Backend::shutdown_signal())
         .await
         .unwrap();
-}
-
-async fn shutdown_signal() {
-    let ctrl_c = async {
-        signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl+C handler");
-    };
-
-    #[cfg(unix)]
-    let terminate = async {
-        signal::unix::signal(signal::unix::SignalKind::terminate())
-            .expect("failed to install signal handler")
-            .recv()
-            .await;
-    };
-
-    #[cfg(not(unix))]
-    let terminate = std::future::pending::<()>();
-
-    tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
-    }
-
-    println!("shutting down");
 }
 
 fn create_s3_storage() -> Arc<RwLock<S3Store>> {
@@ -82,59 +51,4 @@ fn create_s3_storage() -> Arc<RwLock<S3Store>> {
     .unwrap();
     bucket.set_path_style();
     Arc::new(RwLock::new(S3Store::new(bucket)))
-}
-
-async fn get_action<Store: Storage + std::fmt::Debug>(
-    Path(path): Path<String>,
-    State(cache): State<Arc<RwLock<Store>>>,
-) -> impl IntoResponse {
-    if let Some(data) = cache.read().await.get(&format!("ac/{path}")).await {
-        tracing::info!("Responding from cache for ac/{}", path);
-        Ok(data)
-    } else {
-        tracing::info!("ac/{} not in cache", path);
-        Err(StatusCode::NOT_FOUND)
-    }
-}
-async fn put_action<Store: Storage + std::marker::Sync>(
-    Path(path): Path<String>,
-    State(storage): State<Arc<RwLock<Store>>>,
-    body: Bytes,
-) -> impl IntoResponse {
-    if storage.read().await.has(&format!("ac/{path}")).await {
-        StatusCode::CREATED
-    } else {
-        tracing::info!("Storing in cache for ac/{}", path);
-        storage.write().await.set(&format!("ac/{path}"), body.to_vec()).await;
-        StatusCode::CREATED
-    }
-}
-async fn get_item<Store: Storage>(
-    Path(path): Path<String>,
-    State(storage): State<Arc<RwLock<Store>>>,
-) -> impl IntoResponse {
-    if let Some(data) = storage.read().await.get(&format!("cas/{path}")).await {
-        tracing::info!("Responding from cache for cas/{}", path);
-        Ok(data)
-    } else {
-        tracing::info!("cas/{} not in cache", path);
-        Err((StatusCode::NOT_FOUND, "not found"))
-    }
-}
-async fn put_item<Store: Storage + std::marker::Sync>(
-    Path(path): Path<String>,
-    State(storage): State<Arc<RwLock<Store>>>,
-    body: Bytes,
-) -> impl IntoResponse {
-    tracing::info!("Storing in cache for cas/{}", path);
-    if storage.read().await.has(&format!("ac/{path}")).await {
-        StatusCode::CREATED
-    } else {
-        storage
-            .write()
-            .await
-            .set(&format!("cas/{path}"), body.to_vec())
-            .await;
-        StatusCode::CREATED
-    }
 }
